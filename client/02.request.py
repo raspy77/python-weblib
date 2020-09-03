5e001f8fe4df44c5782e39df4250b489b6fbdffe
raspy77 <shk9339@naver.com>
2020-09-03 10:15:12 +0900

    client

---
+++ client/02.request.py
@@ -0,0 +1,33 @@
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
+
+# GET
+http_response = urlopen('https://www.example.com')
+body = http_response.read()
+html = body.decode("utf-8")
+print(html)
+
+# POST
+data = {
+    'id': 'raspy',
+    'pw': '1234'
+}
+data = urlencode(data).encode('utf-8')
+
+request = Request('https://www.example.com', data)
+request.add_header('Content-Type', 'text/html')
+
+http_response = urlopen(request)
+body = http_response.read()
+html = body.decode("utf-8")
+print(html)
+
+
+
+
+
+print(html)
+
+
+
+
